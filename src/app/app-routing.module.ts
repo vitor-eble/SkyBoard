e2fcd4c2eb36e3e09a923238dd37f3b2af502a68
Vitor Eble <vitoreble06@gmail.com>
2025-07-27 14:33:30 -0300

    implementação dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerSearchComponent } from './features/player-search/player-search.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/player-search', pathMatch: 'full' },
-  {path: 'player-search', component: PlayerSearchComponent },
+  { path: 'player-search', component: PlayerSearchComponent },
+  { path: 'profile/:id', component: ProfileComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerSearchComponent } from './features/player-search/player-search.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerSearchComponent
+    PlayerSearchComponent,
+    ProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
