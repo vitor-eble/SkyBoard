268c84de2099278d519ace80f144086a03572348
Vitor Eble <vitoreble06@gmail.com>
2025-07-29 12:49:24 -0300

    teste para tirar o CORS

--- src/app/features/player-search/player-search.component.ts
+++ src/app/features/player-search/player-search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SkyblockService } from '../../services/skyblock.service';
@@ -7,34 +8,40 @@ import { SkyblockService } from '../../services/skyblock.service';
   selector: 'app-player-search',
   standalone: false,
   templateUrl: './player-search.component.html',
-  styleUrl: './player-search.component.css'
+  styleUrl: './player-search.component.css',
 })
-export class PlayerSearchComponent {
-
-  form: FormGroup
+export class PlayerSearchComponent implements OnInit {
+  form!: FormGroup;
+  private destroyRef = inject(DestroyRef);
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private skyBlockService: SkyblockService) {
-      this.form =this.fb.group({
-        playerNickname: ['', Validators.required]
-      })
+    private skyBlockService: SkyblockService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      playerNickname: ['', Validators.required],
+    });
   }
 
-  buscarPerfil(){
-    const nickname = this.form.value.playerNickname;
+  buscarPerfil() {
+    const { playerNickname } = this.form.getRawValue();
 
-    this.skyBlockService.getUUID(nickname).subscribe({
-      next: (uuid: string) => {
-        this.router.navigate(['/profile', uuid]);
-      },
-      error: (err) => {
-        console.error('Error fetching UUID:', err);
-        alert('Error fetching player data. Please check the nickname and try again.');
-      }
-    })
-
-
+    this.skyBlockService
+      .getUUID(playerNickname)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (uuid: string) => {
+          this.router.navigate(['/profile', uuid]);
+        },
+        error: (err) => {
+          console.error('Error fetching UUID:', err);
+          alert(
+            'Error fetching player data. Please check the nickname and try again.'
+          );
+        },
+      });
   }
 }
--- src/app/services/skyblock.service.ts
+++ src/app/services/skyblock.service.ts
@@ -1,25 +1,46 @@
 import { Injectable } from '@angular/core';
 import { environments } from '../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SkyblockService {
-
   private apiKey: string = environments.hypixelApiKey;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getUUID(nickname: string){
-    return this.http.get<any>(`https://playerdb.co/api/player/minecraft/${nickname}`)
-      .pipe(
-        map((res: any) => res.data.player.raw_id)
-      )
+  getUUID(nickname: string) {
+    return this.http
+      .get<any>(`https://playerdb.co/api/player/minecraft/${nickname}`)
+      .pipe(map((res: any) => res.data.player.raw_id));
   }
 
   getSkyblockProfile(uuid: string) {
-    return this.http.get<any>(`https://api.hypixel.net/skyblock/profiles?uuid=${uuid}&key=${this.apiKey}`)
+    console.log('API Key:', this.apiKey);
+
+    const headers = new HttpHeaders()
+      .set('API-Key', this.apiKey)
+      .set('Content-Type', 'application/json');
+
+    console.log('Headers:', headers);
+    console.log(
+      'URL:',
+      `https://api.hypixel.net/v2/skyblock/profiles?uuid=${uuid}`
+    );
+
+    return this.http
+      .get<any>(`https://api.hypixel.net/v2/skyblock/profiles?uuid=${uuid}`, {
+        headers,
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => {
+          console.log('Response headers:', response.headers.keys());
+          console.log('Full response:', response);
+          return response.body;
+        })
+      );
   }
 }
